20baf0251eaaf8e84a1d15f751484ac45ff1fd19
Lukáš Houf <lukas.houf@avast.com>
2021-07-23 13:39:16 +0200

    Test parse Client

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,12 +5,28 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Parse from 'parse';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	created() {
+		Parse.initialize('D1GxqolYkhpwXoIasj6u0mhyZBf39w832PB39kyh', 'hK1irYeW2s7uJ0PhrQhWwTmyaduaLScLfTnvEO6k');
+		Parse.serverURL = "https://parseapi.back4app.com/";
+
+		var user = new Parse.User();
+		user.save({
+			username: "Lukas",
+			email: "lukas.houf@gmail.com",
+			password: "hesloHesl0"
+		}).then(function(response) {
+			alert('New object create with success! ObjectId: ' + response.id + ', '+ user.get('username'));
+		}).catch(function(error) {
+			alert('Error: ' + error.message);
+		});	
+	}
 }
 </script>
 
