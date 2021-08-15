f5666361fcd6eee14e63a6df9eedf1e892273305
Michaela <michaela@michaela-air-3.home>
2021-08-15 12:59:15 +0200

    WIP: categories and products config

---
+++ client/src/assets/config/products.js
@@ -0,0 +1,20 @@
+export const productCategories = [{
+    categoryId: 1,
+    categoryName: "Jeraby",
+    showInNavigation: false
+}]
+
+export const products = [{
+    productId: 1,
+    productCategory: [1],
+    productName: "Jeraby 1",
+    images: [{
+        path: "",
+        name: "",
+        type: "main"
+    }, {
+        path: "",
+        name: "",
+        type: "gen"
+    }]
+}]
